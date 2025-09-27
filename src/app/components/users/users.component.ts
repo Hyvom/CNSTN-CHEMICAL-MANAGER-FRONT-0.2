bd25dc5f106a2c1d9bb50adc126a64bcab922ceb
Hyvom <hyvomrahmouni00@gmail.com>
2025-09-27 22:09:18 +0100

    user component updated

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  users: any[] = [];
+  message: string = '';
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService.getUsers().subscribe({
+      next: (data) => this.users = data,
+      error: (err) => this.message = '❌ Failed to load users: ' + err.message
+    });
+  }
+
+  deleteUser(id: number) {
+    if (confirm('Are you sure you want to delete this user?')) {
+      this.userService.deleteUser(id).subscribe({
+        next: () => {
+          this.message = '🗑️ User deleted successfully';
+          this.loadUsers();
+        },
+        error: (err) => this.message = '❌ Delete failed: ' + err.message
+      });
+    }
+  }
 }
